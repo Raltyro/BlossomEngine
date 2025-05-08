2dc54f56d197850bccd9fe549ec555f103dd10c7
Ralty <78720179+Raltyro@users.noreply.github.com>
2025-05-08 10:07:31 +0700

    Update Shader.hx

--- sourceInternal/openfl/display/Shader.hx
+++ sourceInternal/openfl/display/Shader.hx
@@ -128,7 +128,7 @@ class Shader
 		// Specify the default glVersion.
 		// We can use compile defines to guess the value that prevents crashes in the majority of cases.
 		//return #if lime_opengles "100" #elseif "120" #end;
-		return "200";
+		return "300 es";
 	}
 
 	/**
